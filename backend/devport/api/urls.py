dd19b4b5a3a6a7616bd8cab91b42d328234c531e
Marcin Baranowski <mbaranowski57@gmail.com>
2024-04-29 15:16:16 +0200

    added JWT auth

--- backend/devport/api/urls.py
+++ backend/devport/api/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from api.views import *
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    #auth
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+
+    #entities
     path('profiles', getProfiles),
     path('projects', getProjects),
     path('tags', getTags)
